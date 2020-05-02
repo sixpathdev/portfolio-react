5c308f1d1579334e7e7215c9bf66f9f98e02c542
gabrieltino <miracle8093@gmail.com>
2020-05-02 18:00:28 +0100

    Profile changed

--- src/components/About.js
+++ src/components/About.js
@@ -9,9 +9,8 @@ const About = () => {
           <Profile />
         </div>
               <div id="about_me_text" className="col-12 col-md-5 offset-md-1 mt-5 mt-md-3 text-white">
-                  <p>I'm a Full-stack web developer based in Nigeria.</p>
-                  <p>I'm not yet a pro which I intend to become soon, but I'm capable of learning and working with new technologies swiftly.</p>
-                  <p>At the moment, I'm a back-end developer at Babbage Intelligence, Community Contributor at DSC Uniport.</p>
+              <p>I'm a Full-stack web developer based in Nigeria, working with different web technologies to achieve awesome results while following best practices applicable. I'm capable of learning and working with new technologies swiftly.</p>
+                  <p>At the moment, I'm a full-stack developer at Babbage Intelligence, Community Contributor at DSC Uniport.</p>
                   <p>Most of my time is spent on learning new technologies in the web to build awesome stuffs. I mentor beginners remotely and also help other devs if the need arises.</p>
                   <p>Outside being a techy, I hangout with few friends and try not to be an introvert in reality while being social on social media, <span role="img" aria-label="Smile">😊</span>.</p>
         </div>
