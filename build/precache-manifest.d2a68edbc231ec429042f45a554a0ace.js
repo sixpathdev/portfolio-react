4ed5404ab0d1dd53c2920a8395b34d8866a14915
Miracle Gabriel <sixpathdev@gmailc.com>
2020-05-22 10:54:36 +0100

    New project added

---
+++ build/precache-manifest.d2a68edbc231ec429042f45a554a0ace.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "90a46d281560ceb9b4dec71ee7f83da9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9f2b16cfed05ef1f13be",
+    "url": "/static/css/main.95b69adc.chunk.css"
+  },
+  {
+    "revision": "82b9f6a1609709ac3b8f",
+    "url": "/static/js/2.c448dcce.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.c448dcce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9f2b16cfed05ef1f13be",
+    "url": "/static/js/main.2662065d.chunk.js"
+  },
+  {
+    "revision": "5fcbf2371af52303c5ef",
+    "url": "/static/js/runtime-main.c226d6e8.js"
+  }
+]);
