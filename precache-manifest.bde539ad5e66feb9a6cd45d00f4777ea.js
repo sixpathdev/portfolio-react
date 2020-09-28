13a3a3482f625aad22e7d70f27f036b25597fbbf
Miracle Gabriel <sixpathdev@gmailc.com>
2020-09-28 23:30:54 +0100

    updated

---
+++ precache-manifest.bde539ad5e66feb9a6cd45d00f4777ea.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "28ea08977eb1b8aaf219f170ea358efb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "293e974f284e11f0b81f",
+    "url": "/static/css/main.95b69adc.chunk.css"
+  },
+  {
+    "revision": "82b9f6a1609709ac3b8f",
+    "url": "/static/js/2.c448dcce.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.c448dcce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "293e974f284e11f0b81f",
+    "url": "/static/js/main.b14bd68a.chunk.js"
+  },
+  {
+    "revision": "5fcbf2371af52303c5ef",
+    "url": "/static/js/runtime-main.c226d6e8.js"
+  }
+]);
